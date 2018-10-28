d220f5528220be7bfea2981939513363abfd639d
Henrik <henrik.jp.andersson@hiq.se>
2018-10-28 16:15:51 +0100

    Initial commit - Created project

---
+++ app/src/main/java/com/example/henrikandersson/bbcrssapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.henrikandersson.bbcrssapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
